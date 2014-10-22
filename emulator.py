9af5b326c482c214edb349c4a9e4dbefb8bbd023
Marcelo Veiga Neves <marcelo.veiga@gmail.com>
2014-10-21 22:33:48 -0400

    separte the application launcher code

--- emulator.py
+++ emulator.py
@@ -1,5 +1,6 @@
 from mininet.topo import Topo
-from mininet.node import Controller, RemoteController, OVSKernelSwitch, CPULimitedHost, OVSController
+from mininet.node import (Controller, RemoteController, OVSKernelSwitch,
+                          CPULimitedHost, OVSController)
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.cli import CLI
@@ -15,65 +16,9 @@ import os
 import sys
 import json
 
-from network import DumbbellNet, SingleSwitchNet, SinglepathTreeNet, MultipathTreeNet, FatTreeNet, ShamrockNet
-
-def RunTestHadoop(hosts):
-	basedir = "./hadoop"
-	done = "./output/done.json"
-	emulator = basedir + "/emulator.py"
-
-	if os.path.isfile(done):
-		os.remove(done)
-
-	print "Starting monitor ..."
-	output_dir="output"
-	monitor = multiprocessing.Process(target = monitor_devs_ng, args =
-                ('%s/rate.txt' % output_dir, 0.01))
-	monitor.start()
-
-	print "Running Hadoop simulation ..."
-	for h in hosts:
-		h.popen('%s %s > ./output/output-%s.txt 2> ./output/error-%s.txt' % (emulator, h.IP(), h.IP(), h.IP()), shell = True)
-	while True:
-		if os.path.isfile(done):
-			break
-		sleep(1)
-
-	print "Stopping monitor ..."
-	monitor.terminate()
-        os.system("killall -9 bwm-ng")
-
-	sleep(5)
-	print "Done."
-
-def RunTestIperf(hosts):
-	basedir = "./benchmarks"
-	done = "./output/done.json"
-	emulator = basedir + "/emulator.py"
-
-	if os.path.isfile(done):
-		os.remove(done)
-
-	print "Starting monitor ..."
-	output_dir="output"
-	monitor = multiprocessing.Process(target = monitor_devs_ng, args =
-                ('%s/rate.txt' % output_dir, 0.01))
-	monitor.start()
-
-	print "Running tests ..."
-	for h in hosts:
-		h.popen('%s %s > ./output/output-%s.txt 2> ./output/error-%s.txt' % (emulator, h.IP(), h.IP(), h.IP()), shell = True)
-	while True:
-		if os.path.isfile(done):
-			break
-		sleep(1)
-
-	print "Stopping monitor ..."
-	monitor.terminate()
-        os.system("killall -9 bwm-ng")
-
-	sleep(5)
-	print "Done."
+from network import (DumbbellNet, SingleSwitchNet, SinglepathTreeNet,
+                     MultipathTreeNet, FatTreeNet, ShamrockNet)
+from applauncher import HadoopTest, IperfTest
 
 def RunTest(net=None, remoteController=False):
 	net.start()
@@ -95,8 +40,8 @@ def RunTest(net=None, remoteController=False):
 
 	#CLI(net)
 
-        RunTestHadoop(hosts)
-        #RunTestIperf(hosts)
+	HadoopTest(hosts)
+	#IperfTest(hosts)
 
 	#CLI(net)
 	net.stop()
