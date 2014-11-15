e3fc8b288de980b95d7f08b07e125350b6aed137
Marcelo Veiga Neves <marcelo.veiga@gmail.com>
2014-11-14 22:17:27 -0500

    improve cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -2,41 +2,39 @@
 
 sudo mn -c
 
+kill_them_all()
+{
+	pids=$*
+	#echo $pids
+	if [ ! -z "$pids" ]
+	then
+		sudo kill -9 $pids
+	fi
+}
+
 x=`ps auxf | grep emulator.py | grep mnexec | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
 
 x=`ps auxf | grep emulator.py | grep python | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
 
 x=`ps axuf | grep iperf | grep "/bin/sh" | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
 
 x=`ps auxf | grep iperf | sed '/grep/d' | sed '/geany/d' | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
 
 x=`ps axuf | grep bwm-ng | grep "/bin/sh" | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
 
 x=`ps auxf | grep bwm-ng | sed '/grep/d' | awk '{print $2}'`
-if [ ! -z "$x" ]
-then
-    sudo kill -9 $x
-fi
+kill_them_all $x
+
+x=`ps axuf | grep sshd | grep banner | sed '/grep/d' | awk '{print $2}'`
+kill_them_all $x
+
+x=`ps axuf | grep com.company.Main | sed '/grep/d' | awk '{print $2}'`
+kill_them_all $x
 
 rm ./hadoop/logs/hadoop-*.log -f
 rm ./hadoop/logs/simulator-*.log -f
